d1f504bf83f1de12e843b15223636b5d8ab9fccf
Albert Weinert <info@der-albert.com>
2011-05-15 17:45:49 +0200

    added simple build all

--- psake.ps1
+++ psake.ps1
@@ -1,5 +1,5 @@
 # Helper script for those who want to run
 # psake without importing the module.
 import-module .\tools\psake\psake.psm1
-invoke-psake @args
+invoke-psake  -framework '4.0' @args 
 remove-module psake
