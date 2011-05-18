82c2dd70f3b3ae120da67a2024f83c1d6a61c793
Albert Weinert <info@der-albert.com>
2011-05-18 17:30:17 +0200

    created nuget

--- src/FrameworkVersion.cs
+++ src/FrameworkVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.0.23")]
-[assembly: AssemblyFileVersion("0.2.0.23")]
+[assembly: AssemblyVersion("0.2.0.25")]
+[assembly: AssemblyFileVersion("0.2.0.25")]
