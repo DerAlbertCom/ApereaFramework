d1f2f5fdd1f8179456f39885172566f056090740
Albert Weinert <info@der-albert.com>
2011-08-01 15:13:13 +0200

    updated to ravendb 427

--- src/FrameworkVersion.cs
+++ src/FrameworkVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.1.59")]
-[assembly: AssemblyFileVersion("0.6.1.59")]
+[assembly: AssemblyVersion("0.6.1.60")]
+[assembly: AssemblyFileVersion("0.6.1.60")]
