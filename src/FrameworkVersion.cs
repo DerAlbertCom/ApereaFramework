1d35ce28af215ca118753e6d38b0a8adfa5638eb
Albert Weinert <info@der-albert.com>
2013-07-27 15:56:15 +0200

    bumped revision

--- src/FrameworkVersion.cs
+++ src/FrameworkVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.3.74")]
-[assembly: AssemblyFileVersion("0.7.3.74")]
+[assembly: AssemblyVersion("0.7.3.75")]
+[assembly: AssemblyFileVersion("0.7.3.75")]
