8f0573fd8457a86723d3691f6c6ac1cd3e1dacae
Albert Weinert <info@der-albert.com>
2013-10-18 12:05:09 +0200

    updated to support MVC4 and 5 and EntityFramewoork 6

--- src/FrameworkVersion.cs
+++ src/FrameworkVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.3.77")]
-[assembly: AssemblyFileVersion("0.7.3.77")]
+[assembly: AssemblyVersion("0.7.3.79")]
+[assembly: AssemblyFileVersion("0.7.3.79")]
