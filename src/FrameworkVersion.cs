01ce95197161f5e4c087af51b8da73163111f01f
Albert Weinert <info@der-albert.com>
2014-03-02 22:44:43 +0100

    0.7.3.80

--- src/FrameworkVersion.cs
+++ src/FrameworkVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.3.80")]
-[assembly: AssemblyFileVersion("0.7.3.80")]
+[assembly: AssemblyVersion("0.7.3.81")]
+[assembly: AssemblyFileVersion("0.7.3.81")]
